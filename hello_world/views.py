b391e1d60e48cb580449d52d1c30efcf8043c558
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-02 18:36:04 +0300

    Add views processes

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method) 
